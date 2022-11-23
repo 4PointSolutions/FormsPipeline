3fde8d006a85cd06aa40c76ccb30c4c253807ac0
Rob McDougall <rmcdougawww@rogers.com>
2022-11-23 15:59:32 -0500

    Remove type information from sealed interface to fix build.
    Eclipse wants the information included, but JDK compiler does not.

--- formspipeline/src/main/java/com/_4point/aem/formspipeline/api/DataTransformation.java
+++ formspipeline/src/main/java/com/_4point/aem/formspipeline/api/DataTransformation.java
@@ -8,9 +8,10 @@ import com._4point.aem.formspipeline.api.DataTransformation.*;
  * This is a "tagging" interface that is the parent of all the DataTransformation interfaces.
  *
  */
-public sealed interface DataTransformation permits DataTransformationOneToOne<? extends DataChunk<? extends Context>, ? extends DataChunk<? extends Context>>, 
-												   DataTransformationOneToMany<? extends DataChunk<? extends Context>>, 
-												   DataTransformationManyToOne<? extends DataChunk<? extends Context>>, 
+@SuppressWarnings("rawtypes")
+public sealed interface DataTransformation permits DataTransformationOneToOne, 
+												   DataTransformationOneToMany, 
+												   DataTransformationManyToOne, 
 												   DataTransformationManyToMany 
 												   {
 	
--- formspipeline/src/main/java/com/_4point/aem/formspipeline/api/OutputTransformation.java
+++ formspipeline/src/main/java/com/_4point/aem/formspipeline/api/OutputTransformation.java
@@ -8,9 +8,10 @@ import com._4point.aem.formspipeline.api.OutputTransformation.*;
  * This is a "tagging" interface that is the parent of all the OutputTransformation interfaces.
  *
  */
-public sealed interface OutputTransformation permits OutputTransformationOneToOne<? extends OutputChunk<? extends Context, ? extends Context>, ? extends OutputChunk<? extends Context, ? extends Context>>,
-													 OutputTransformationOneToMany<? extends OutputChunk<? extends Context, ? extends Context>>,
-													 OutputTransformationManyToOne<? extends OutputChunk<? extends Context, ? extends Context>>,
+@SuppressWarnings("rawtypes")
+public sealed interface OutputTransformation permits OutputTransformationOneToOne,
+													 OutputTransformationOneToMany,
+													 OutputTransformationManyToOne,
 													 OutputTransformationManyToMany
 													 {
 
