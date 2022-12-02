797e932e0920c7b91bf2c46be87c916fe7efe62b
lien.ly <lien.ly@FPS-PF1CFRQY.4PTSolutions.local>
2022-12-02 17:10:50 -0500

    Initial version of XmlDataChunk implementation

--- spring-starter/src/main/java/com/_4point/aem/formspipeline/spring/chunks/XmlDataChunk.java
+++ spring-starter/src/main/java/com/_4point/aem/formspipeline/spring/chunks/XmlDataChunk.java
@@ -1,16 +1,91 @@
 package com._4point.aem.formspipeline.spring.chunks;
 
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import com._4point.aem.formspipeline.api.Context;
 import com._4point.aem.formspipeline.api.DataChunk;
 
-public abstract class XmlDataChunk<T extends Context> implements DataChunk<T> {
-
-	@Override
-	public byte[] bytes() {
-		// TODO Auto-generated method stub
-		return null;
+public class XmlDataChunk<T extends Context> implements DataChunk<T> {
+	
+	private static final Logger logger = LoggerFactory.getLogger(XmlDataChunk.class);
+	
+	private Document xmlDoc;
+	private String xmlString;
+	private XPath xpathFactory;
+	
+	public XmlDataChunk() {
+		XPathFactory xPathF = XPathFactory.newInstance();
+		xpathFactory = xPathF.newXPath();	
 	}
 
 	@Override
-	abstract public T dataContext();
+	public byte[] bytes() 
+	{
+		return xmlString.getBytes();		
+	}
+
+	@Override
+	public T dataContext() {
+		return null;
+	}
+	
+	public void loadXMLFromString(String xml) throws Exception
+	{		
+		xmlString = xml;
+	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	    DocumentBuilder builder = factory.newDocumentBuilder();
+	    Document doc = builder.parse(this.asInputStream());
+	    doc.getDocumentElement().normalize();
+	    xmlDoc = doc;	  	    
+	}
+	
+	public List<String> getXmlDataValue(String xpath) {
+		List<String> list = new ArrayList<>();
+		try {
+	        //create XPathExpression object
+			XPathExpression expr = xpathFactory.compile(xpath);			
+	        //evaluate expression result on XML document
+	        NodeList nodes = (NodeList) expr.evaluate(xmlDoc, XPathConstants.NODESET);
+	        for (int i = 0; i < nodes.getLength(); i++) {
+	        	Node node = nodes.item(i);
+	        	if (node.getNodeType() == Node.ELEMENT_NODE) {
+	        		Element childElementDetails = (Element)node;
+	        		if(childElementDetails.getAttribute("value").isBlank()) {
+        				list.add(childElementDetails.getTextContent());
+	        		} else {
+	        			list.add(childElementDetails.getAttribute("value"));	
+	        		}
+	        	}	            
+	        }
+		} catch (Exception e) {
+			
+			Formatter fmt = new Formatter();
+			logger.error(fmt.format("Failed to parse xml path (%s). Error message %s", xpath, e.getMessage()).toString());
+		}
+		
+		if(list.isEmpty()) {
+			list.add(xpath);  	        //If it couldn't find the value (i.e pointing to something that isn't text)
+		}
+		return list;		
+	}
+	
+	public Element getXmlElement(String xpath) {
+		return xmlDoc.getElementById(xpath);
+	}
 }
--- spring-starter/src/test/java/com/_4point/aem/formspipeline/spring/chunks/XmlDataChunkTest.java
+++ spring-starter/src/test/java/com/_4point/aem/formspipeline/spring/chunks/XmlDataChunkTest.java
@@ -2,20 +2,139 @@ package com._4point.aem.formspipeline.spring.chunks;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
-@Disabled
 class XmlDataChunkTest {
+	
+	String SIMPLE_XML_DATA_FILE = "/xml/laptop.xml";
+	String COMPLEX_XML_DATA_FILE = "/xml/English/R42565_FB0470022B_16161694_T2_4.xml";
+	String xmlDataAsString;
+	
+	XmlDataChunk xmlDataChunk;
+	
+    @BeforeEach
+    protected void setUp() {
+		try {
+			
+			xmlDataChunk = new XmlDataChunk();
+					
+			ClassPathResource resource = new org.springframework.core.io.ClassPathResource(SIMPLE_XML_DATA_FILE);
+			List<String> content = Files.readAllLines(Paths.get(
+					resource.getURI()), Charset.defaultCharset());
+                            
+			String s = content.stream().map(e -> e.toString()).reduce("", String::concat);
+			System.out.println("XML data\n\n" + s);
+			xmlDataChunk.loadXMLFromString(s);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+           	
+    }
+    
+    @Test
+    void testGetDataForXpath_simpleXML_FoundItemPrice_return1000() {
+    	String xpath = "/laptops/laptop[@name='Dell']/price";
+    	List<String> EXPECTED_VALUE = Arrays.asList("1000");
+        
+        List<String> actualValue = xmlDataChunk.getXmlDataValue(xpath);
+    	System.out.println("actualValue " + actualValue);
+    	assertEquals(EXPECTED_VALUE,actualValue);   	
+    }
+    
+    @Test
+    void testGetDataForXpath_simpleXML_FoundAllInstance_returnList() {
+    	String xpath = "/laptops/laptop/price";
+    	List<String> EXPECTED_VALUE = Arrays.asList("2000","1000","4000");
+        
+        List<String> actualValue = xmlDataChunk.getXmlDataValue(xpath);
+    	System.out.println("actualValue " + actualValue);
+    	assertEquals(EXPECTED_VALUE,actualValue);   	
+    }
+    
+    @Test
+    void testGetDataForXpath_simpleXML_NotFound_returnEmpty() {
+    	String xpath = "/laptops/laptop[@name='Thinkpad']/price";
+    	List<String> EXPECTED_VALUE = Arrays.asList(xpath); 
+        
+        List<String> actualValue = xmlDataChunk.getXmlDataValue(xpath);
+    	System.out.println("actualValue " + actualValue);
+    	assertEquals(EXPECTED_VALUE,actualValue);   	
+    }
+    
+    @Test
+    void testGetDataForXpath_complexXML_FoundRepeatItem_returnValue() {
+    	XmlDataChunk complexXmlDataChunk = new XmlDataChunk();
+		
+		ClassPathResource resource = new org.springframework.core.io.ClassPathResource(COMPLEX_XML_DATA_FILE);
+		List<String> content;
+		try {
+			content = Files.readAllLines(Paths.get(resource.getURI()), Charset.defaultCharset());
+			String s = content.stream().map(e -> e.toString()).reduce("", String::concat);
+			System.out.println("\n\nXML data\n" + s);
+			complexXmlDataChunk.loadXMLFromString(s);
+			
+			String xpath = "/E1_Output/XMLInvoices/XMLInvoice/DriverSection/DocumentType_4";
+			List<String> EXPECTED_VALUE = Arrays.asList("INVS01","INVS01","INVS01","INVS01");
+	        
+	        List<String> actualValue = complexXmlDataChunk.getXmlDataValue(xpath);
+	    	System.out.println("testComplexXml_FoundItem_returnValue actualValue " + actualValue);
+	    	assertEquals(EXPECTED_VALUE,actualValue);  
+			
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    }
 
-	@Test
-	void testBytes() {
-		fail("Not yet implemented");
-	}
+    @Test
+    void testComplexXml_FoundSingleRepeatItem_returnValue() {
+    	XmlDataChunk complexXmlDataChunk = new XmlDataChunk();
+		
+		ClassPathResource resource = new org.springframework.core.io.ClassPathResource(COMPLEX_XML_DATA_FILE);
+		List<String> content;
+		try {
+			content = Files.readAllLines(Paths.get(resource.getURI()), Charset.defaultCharset());
+			String s = content.stream().map(e -> e.toString()).reduce("", String::concat);
+			System.out.println("\n\nXML data\n" + s);
+			complexXmlDataChunk.loadXMLFromString(s);
+			
+			String xpath = "/E1_Output/XMLInvoices/XMLInvoice[1]/DriverSection/DocumentType_4";
+			List<String> EXPECTED_VALUE = Arrays.asList("INVS01");
+	        
+	        List<String> actualValue = complexXmlDataChunk.getXmlDataValue(xpath);
+	    	System.out.println("testComplexXml_FoundItem_returnValue actualValue " + actualValue);
+	    	assertEquals(EXPECTED_VALUE,actualValue);  
+			
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    }
 
-	@Test
-	void testDataContext() {
-		fail("Not yet implemented");
-	}
+    
+    
 
 }
