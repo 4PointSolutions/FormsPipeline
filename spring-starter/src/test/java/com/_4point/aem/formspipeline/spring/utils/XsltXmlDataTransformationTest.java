1cdd31317d65b9c0b77a2fb627533f0ee9162be0
Rob McDougall <rmcdougawww@rogers.com>
2022-12-06 12:38:55 -0500

    Updated XML Transformation tests to utilize XmlUnit.
    By using XmlUnit this should eliminate issues with line ending
    differences across platforms.

--- spring-starter/src/test/java/com/_4point/aem/formspipeline/spring/utils/XsltXmlDataTransformationTest.java
+++ spring-starter/src/test/java/com/_4point/aem/formspipeline/spring/utils/XsltXmlDataTransformationTest.java
@@ -1,7 +1,9 @@
 package com._4point.aem.formspipeline.spring.utils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+import static org.xmlunit.matchers.CompareMatcher.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -13,6 +15,7 @@ import javax.xml.transform.stream.StreamSource;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.xmlunit.builder.Input;
 
 import com._4point.aem.formspipeline.api.Context;
 import com._4point.aem.formspipeline.api.DataChunk;
@@ -23,22 +26,22 @@ class XsltXmlDataTransformationTest {
 	private final byte[] xsltBytes = TestHelper.getFileBytesFromResource(TestHelper.SIMPLE_XSLT_DATA_FILE);
 	private byte[] invalidXsltBytes = TestHelper.getFileBytesFromResource(TestHelper.INVALID_XSLT_DATA_FILE);
 		
-	private final String EXPECTED_TRANSFORMED_XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><laptops>\r\n"
-			+ "	<laptop name=\"Lenonvo\">\r\n"
-			+ "		<price>2000</price>\r\n"
-			+ "		<MEMORY>250GB</MEMORY>\r\n"
-			+ "		<hardDrive>2GB</hardDrive>\r\n"
-			+ "	</laptop>\r\n"
-			+ "	<laptop name=\"Dell\">\r\n"
-			+ "		<price>1000</price>\r\n"
-			+ "		<MEMORY>100GB</MEMORY>\r\n"
-			+ "		<hardDrive value=\"1GB\"/>\r\n"
-			+ "	</laptop>\r\n"
-			+ "	<laptop name=\"Apple\">\r\n"
-			+ "		<price>4000</price>\r\n"
-			+ "		<MEMORY>400GB</MEMORY>\r\n"
-			+ "		<hardDrive>4GB</hardDrive>\r\n"
-			+ "	</laptop>\r\n"
+	private final String EXPECTED_TRANSFORMED_XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><laptops>\n"
+			+ "	<laptop name=\"Lenonvo\">\n"
+			+ "		<price>2000</price>\n"
+			+ "		<MEMORY>250GB</MEMORY>\n"
+			+ "		<hardDrive>2GB</hardDrive>\n"
+			+ "	</laptop>\n"
+			+ "	<laptop name=\"Dell\">\n"
+			+ "		<price>1000</price>\n"
+			+ "		<MEMORY>100GB</MEMORY>\n"
+			+ "		<hardDrive value=\"1GB\"/>\n"
+			+ "	</laptop>\n"
+			+ "	<laptop name=\"Apple\">\n"
+			+ "		<price>4000</price>\n"
+			+ "		<MEMORY>400GB</MEMORY>\n"
+			+ "		<hardDrive>4GB</hardDrive>\n"
+			+ "	</laptop>\n"
 			+ "</laptops>";
 	
 	@BeforeEach
@@ -52,26 +55,20 @@ class XsltXmlDataTransformationTest {
 		InputStream xmlDoc = TestHelper.getFileFromResource(TestHelper.SIMPLE_XML_DATA_FILE);
 		Source source = new StreamSource(xmlDoc);
 		ByteArrayOutputStream output = new ByteArrayOutputStream(); 				
-		assertThrows( TransformerException.class, () -> {
-			xmlTransformer.transform(source, output);
-		});	
+		assertThrows( TransformerException.class, ()->xmlTransformer.transform(source, output));	
 	}
 	
 	@Test
-	void testTransform_success()  {
+	void testTransform_success() throws Exception  {
 		XsltXmlDataTransformation xmlTransformer = new XsltXmlDataTransformation(xsltBytes,xmlBytes);	
 		
 		InputStream xmlDoc = TestHelper.getFileFromResource(TestHelper.SIMPLE_XML_DATA_FILE);
 		Source source = new StreamSource(xmlDoc);
 		ByteArrayOutputStream output = new ByteArrayOutputStream(); 		
 		
-		try {			
 			xmlTransformer.transform(source, output);
 			String transformedXML = new String(output.toByteArray(), StandardCharsets.UTF_8);
-			assertEquals(EXPECTED_TRANSFORMED_XML,transformedXML);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		} 
+			assertThat(transformedXML, isIdenticalTo(Input.fromString(EXPECTED_TRANSFORMED_XML)));
 	}
 	
 	@Test
@@ -80,6 +77,7 @@ class XsltXmlDataTransformationTest {
 		
 		//Something not right here, needs to be re-factored.
 		DataChunk<Context> data = xmlTransformer.process(xmlTransformer);
-		assertEquals(EXPECTED_TRANSFORMED_XML,new String(data.bytes(), StandardCharsets.UTF_8));
+//		assertEquals(EXPECTED_TRANSFORMED_XML,new String(data.bytes(), StandardCharsets.UTF_8));
+		assertThat(Input.fromByteArray(data.bytes()), isIdenticalTo(Input.fromString(EXPECTED_TRANSFORMED_XML)));
 	}
 }
