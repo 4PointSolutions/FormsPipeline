13298def1d2cdec60bda5da56b2c419f64cefa2f
lien.ly <lien.ly@4point.com>
2022-12-15 10:33:23 -0500

    Code cleanup

--- formspipeline/src/test/java/com/_4point/aem/formspipeline/destinations/OutputDestinationRouterTest.java
+++ formspipeline/src/test/java/com/_4point/aem/formspipeline/destinations/OutputDestinationRouterTest.java
@@ -30,17 +30,17 @@ class OutputDestinationRouterTest {
 	
 	private static EmptyContext mockContext1;	
 	private static OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>> mockOutputDestination1;
-	private static Result<Context, Context, Context> result1;
+	private static Result<Context, Context, Context> mockResult1;
 	
 	private static EmptyContext mockContext2;
 	private static OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>> mockOutputDestination2;
-	private static Result<Context, Context, Context> result2;
+	private static Result<Context, Context, Context> mockResult2;
 	
 	private static OutputChunk<Context, Context> mockOutputChunk;
 	
 	enum TestScenarios {
-		GET_FIRST_ITEM ((a)-> 0, mockOutputDestination1, mockOutputDestination2, result1),
-		GET_SECOND_ITEM ((a)-> 1, mockOutputDestination1, mockOutputDestination2, result2);
+		GET_FIRST_ITEM ((a)-> 0, mockOutputDestination1, mockOutputDestination2, mockResult1),
+		GET_SECOND_ITEM ((a)-> 1, mockOutputDestination1, mockOutputDestination2, mockResult2);
 		//GET_OUTOFBOUND_ITEM ((a)-> 2, mockOutputDestination1, mockOutputDestination2, null);
 		
 		Function<Context,Integer> aFunction;
@@ -59,37 +59,35 @@ class OutputDestinationRouterTest {
 		}
 	}
 		
-	@BeforeAll
+	@BeforeAll	
 	@SuppressWarnings("unchecked")
 	static void mockSetup() {
 		mockOutputChunk = Mockito.mock(OutputChunk.class);
 		
 		mockContext1 = Mockito.mock(EmptyContext.class);
-		result1 = Mockito.mock(Result.class);
+		mockResult1 = Mockito.mock(Result.class);
 		mockOutputDestination1 = Mockito.mock(OutputDestination.class);
-		Mockito.when(mockContext1.getString(Mockito.any())).thenReturn(Optional.of("Data1"));		
-		Mockito.when(result1.dataContext()).thenReturn(mockContext1);
-		Mockito.when(mockOutputDestination1.process(Mockito.any())).thenReturn(result1);
+		Mockito.when(mockContext1.getString(Mockito.anyString())).thenReturn(Optional.of("Data1"));		
+		Mockito.when(mockResult1.dataContext()).thenReturn(mockContext1);
+		Mockito.when(mockOutputDestination1.process(mockOutputChunk)).thenReturn(mockResult1);
 
 		mockContext2 = Mockito.mock(EmptyContext.class);
-		result2 = Mockito.mock(Result.class);
+		mockResult2 = Mockito.mock(Result.class);
 		mockOutputDestination2 = Mockito.mock(OutputDestination.class);
-		Mockito.when(mockContext2.getString(Mockito.any())).thenReturn(Optional.of("Data2"));
-		Mockito.when(result2.dataContext()).thenReturn(mockContext2);
-		Mockito.when(mockOutputDestination2.process(Mockito.any())).thenReturn(result2);		
+		Mockito.when(mockContext2.getString(Mockito.anyString())).thenReturn(Optional.of("Data2"));
+		Mockito.when(mockResult2.dataContext()).thenReturn(mockContext2);
+		Mockito.when(mockOutputDestination2.process(mockOutputChunk)).thenReturn(mockResult2);		
 	}	
 	
-	@DisplayName("Successfully execute process for each OutputDestination")
+	@DisplayName("Test execute PROCESS for each OutputDestination successfully")
 	@ParameterizedTest(name="{index}=> TestScenario=''{0}''")
 	@EnumSource(TestScenarios.class)
 	void testProcess_retrieveElements_Success(TestScenarios scenario) {
-		OutputDestinationRouter<Context, Context, Context> router = new OutputDestinationRouter<Context, Context, Context>
+		OutputDestinationRouter<Context, Context, Context> underTest = new OutputDestinationRouter<Context, Context, Context>
 				(scenario.aFunction, scenario.destination1,scenario.destination2);
 		
-		assertNotNull(router);				
-		Result<Context,Context, ?> results = router.process(mockOutputChunk);
+		Result<Context,Context, ?> results = underTest.process(mockOutputChunk);
 		assertNotNull(results);
-		System.out.println(results.dataContext().getString("anyTextWillWork"));
 		assertSame(scenario.expectedDesults.dataContext(),results.dataContext());		
 		assertSame(scenario.expectedDesults.dataContext().getString("t"),results.dataContext().getString("anyTextWillWork"));	
 	}	
@@ -97,10 +95,10 @@ class OutputDestinationRouterTest {
 	@SuppressWarnings("unchecked")
 	@Test
 	void testProcess_throws_IndexOutOfBoundsException() {	
-		OutputDestinationRouter<Context, Context, Context> router = new OutputDestinationRouter<Context, Context, Context>
+		OutputDestinationRouter<Context, Context, Context> underTest = new OutputDestinationRouter<Context, Context, Context>
 		((a)-> 2, Mockito.mock(OutputDestination.class), Mockito.mock(OutputDestination.class));
 		
-		IndexOutOfBoundsException ex = assertThrows(IndexOutOfBoundsException.class, ()->router.process(mockOutputChunk));
+		IndexOutOfBoundsException ex = assertThrows(IndexOutOfBoundsException.class, ()->underTest.process(mockOutputChunk));
 		String msg = ex.getMessage();
 		assertNotNull(msg);		
 		assertThat(msg, allOf(containsString("Index 2 out of bounds for length 2")));
