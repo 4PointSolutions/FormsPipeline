cbc1c616da9f030dae905708089fb95a21dd0d48
lien.ly <lien.ly@4point.com>
2022-12-15 12:23:46 -0500

    Refactored to make use of @Mock annotation instead of using @BeforeEach method.

--- formspipeline/src/test/java/com/_4point/aem/formspipeline/destinations/OutputDestinationRouterTest.java
+++ formspipeline/src/test/java/com/_4point/aem/formspipeline/destinations/OutputDestinationRouterTest.java
@@ -6,16 +6,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.lenient;
 
-import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -28,30 +29,35 @@ import com._4point.aem.formspipeline.contexts.EmptyContext;
 @ExtendWith(MockitoExtension.class)
 class OutputDestinationRouterTest {	
 	
-	private static EmptyContext mockContext1;	
-	private static OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>> mockOutputDestination1;
-	private static Result<Context, Context, Context> mockResult1;
+	@Mock private static EmptyContext mockContext1;	
+	@Mock private static OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>> mockOutputDestination1;
+	@Mock private static Result<Context, Context, Context> mockResult1;
 	
-	private static EmptyContext mockContext2;
-	private static OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>> mockOutputDestination2;
-	private static Result<Context, Context, Context> mockResult2;
+	@Mock private static EmptyContext mockContext2;
+	@Mock private static OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>> mockOutputDestination2;
+	@Mock private static Result<Context, Context, Context> mockResult2;
 	
-	private static OutputChunk<Context, Context> mockOutputChunk;
+	@Mock private static OutputChunk<Context, Context> mockOutputChunk;
+
+	//Needed because when enum is initialized the mocks are not ready.
+	private static Supplier<OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>>> supplierOutputDestination1 = () -> mockOutputDestination1;
+	private static Supplier<OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>>> supplierOutputDestination2 = () -> mockOutputDestination2;
+	private static Supplier<Result<Context, Context, ?>> supplierResult1 = () -> mockResult1;
+	private static Supplier<Result<Context, Context, ?>> supplierResult2 = () -> mockResult2;
 	
 	enum TestScenarios {
-		GET_FIRST_ITEM ((a)-> 0, mockOutputDestination1, mockOutputDestination2, mockResult1),
-		GET_SECOND_ITEM ((a)-> 1, mockOutputDestination1, mockOutputDestination2, mockResult2);
-		//GET_OUTOFBOUND_ITEM ((a)-> 2, mockOutputDestination1, mockOutputDestination2, null);
+		GET_FIRST_ITEM ((a)-> 0, supplierOutputDestination1, supplierOutputDestination2, supplierResult1),
+		GET_SECOND_ITEM ((a)-> 1, supplierOutputDestination1, supplierOutputDestination2, supplierResult2);
 		
 		Function<Context,Integer> aFunction;
-		OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>> destination1;
-		OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>> destination2;
-		Result<Context,Context, ?> expectedDesults;
+		Supplier<OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>>> destination1;
+		Supplier<OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>>> destination2;
+		Supplier<Result<Context,Context, ?>> expectedDesults;
 		
 		private TestScenarios(Function<Context,Integer> func, 
-				OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>> dest1,
-				OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>> dest2,
-				Result<Context,Context, ?> result) {			
+				Supplier<OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>>> dest1,
+				Supplier<OutputDestination<OutputChunk<Context, Context>,Result<Context, Context, Context>>> dest2,
+				Supplier<Result<Context,Context, ?>> result) {			
 			aFunction = func;
 			destination1 = dest1;
 			destination2 = dest2;	
@@ -59,37 +65,22 @@ class OutputDestinationRouterTest {
 		}
 	}
 		
-	@BeforeAll	
-	@SuppressWarnings("unchecked")
-	static void mockSetup() {
-		mockOutputChunk = Mockito.mock(OutputChunk.class);
-		
-		mockContext1 = Mockito.mock(EmptyContext.class);
-		mockResult1 = Mockito.mock(Result.class);
-		mockOutputDestination1 = Mockito.mock(OutputDestination.class);
-		Mockito.when(mockContext1.getString(Mockito.anyString())).thenReturn(Optional.of("Data1"));		
-		Mockito.when(mockResult1.dataContext()).thenReturn(mockContext1);
-		Mockito.when(mockOutputDestination1.process(mockOutputChunk)).thenReturn(mockResult1);
-
-		mockContext2 = Mockito.mock(EmptyContext.class);
-		mockResult2 = Mockito.mock(Result.class);
-		mockOutputDestination2 = Mockito.mock(OutputDestination.class);
-		Mockito.when(mockContext2.getString(Mockito.anyString())).thenReturn(Optional.of("Data2"));
-		Mockito.when(mockResult2.dataContext()).thenReturn(mockContext2);
-		Mockito.when(mockOutputDestination2.process(mockOutputChunk)).thenReturn(mockResult2);		
-	}	
-	
-	@DisplayName("Test execute PROCESS for each OutputDestination successfully")
+	@DisplayName("Test process and execute OutputDestination's process successfully")
 	@ParameterizedTest(name="{index}=> TestScenario=''{0}''")
 	@EnumSource(TestScenarios.class)
+	//this test loops through each enum.
 	void testProcess_retrieveElements_Success(TestScenarios scenario) {
+		//used lenient here because depending which element is currently being tested the other element won't be checked,
+		//this avoids the unnecessary stubbing exception		
+		lenient().when(mockOutputDestination1.process(mockOutputChunk)).thenReturn(mockResult1);
+		lenient().when(mockOutputDestination2.process(mockOutputChunk)).thenReturn(mockResult2);		
+
 		OutputDestinationRouter<Context, Context, Context> underTest = new OutputDestinationRouter<Context, Context, Context>
-				(scenario.aFunction, scenario.destination1,scenario.destination2);
+				(scenario.aFunction, scenario.destination1.get(),scenario.destination2.get());
 		
 		Result<Context,Context, ?> results = underTest.process(mockOutputChunk);
 		assertNotNull(results);
-		assertSame(scenario.expectedDesults.dataContext(),results.dataContext());		
-		assertSame(scenario.expectedDesults.dataContext().getString("t"),results.dataContext().getString("anyTextWillWork"));	
+		assertSame(scenario.expectedDesults.get(), results);
 	}	
 	
 	@SuppressWarnings("unchecked")
