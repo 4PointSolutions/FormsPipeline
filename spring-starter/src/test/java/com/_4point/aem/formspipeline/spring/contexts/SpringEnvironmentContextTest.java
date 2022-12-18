1f43f095cdd6d816d2ec23fb854b15949a322899
lien.ly <lien.ly@4point.com>
2022-12-18 18:49:59 -0500

    Got the failed tests working.

--- spring-starter/src/test/java/com/_4point/aem/formspipeline/spring/contexts/SpringEnvironmentContextTest.java
+++ spring-starter/src/test/java/com/_4point/aem/formspipeline/spring/contexts/SpringEnvironmentContextTest.java
@@ -1,18 +1,27 @@
 package com._4point.aem.formspipeline.spring.contexts;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.convert.ConversionFailedException;
 
 @SpringBootTest(classes=SpringEnvironmentContext.class)
+// Choose properties that should existing on any developer or server machines and where values wouldn't 
+// or shouldn't be different based on environment
 class SpringEnvironmentContextTest {
 	
     @Autowired
@@ -21,33 +30,33 @@ class SpringEnvironmentContextTest {
 	@BeforeEach
 	void setup() {
 	}
-
-	@Test
-	void testGet_stringProperty_returnNonStringValues() throws MalformedURLException {	
-		assertNotNull(underTest);
-		
-		assertEquals(false, underTest.get("spring.jmx.enabled", Boolean.class).get());
-//		assertEquals(new URL("https://www.4point.com/content/4point/us/en/home.html"), underTest.get("java.vendor.url", URL.class).get());
-	}
 	
 	@Test
 	void testGet_KeyNotFound() {		
 		assertEquals(Optional.empty(), underTest.get("test", String.class));
 	}
 
-	@Disabled("Doesn't work yet, still work in progress.")
 	@Test
 	void testGet_WrongType() {
-		assertEquals(Optional.empty(), underTest.get("os.name", Integer.class));		
+		ConversionFailedException ex = assertThrows(ConversionFailedException.class, ()->underTest.get("USERDOMAIN_ROAMINGPROFILE", Integer.class));
+		String msg = ex.getMessage();
+		assertNotNull(msg);		
+		assertThat(msg, allOf(containsString("Failed to convert from type [java.lang.String] to type [java.lang.Integer]")));		
 	}
-	
-	@Disabled("Doesn't work yet, still work in progress.")
+
+	@Test
+	void testGet_nonStringProperty_returnNonStringValues() throws MalformedURLException {	
+		assertEquals(false, underTest.get("spring.jmx.enabled", Boolean.class).get());		
+		assertEquals(new URL("https://adoptium.net/"), underTest.get("java.vendor.url", URL.class).get());
+		assertThat(underTest.get("os.version", Float.class).get(), is(instanceOf(Float.class)));
+		assertThat(underTest.get("java.vm.specification.version", Integer.class).get(), is(instanceOf(Integer.class)));  
+	}
+
 	@Test
 	void testGet_stringProperty_returnStringValue() {		
-		assertEquals("Windows 11", underTest.get("os.name", String.class).get());
-		assertEquals("17.0.4.1+1", underTest.get("java.specification.version", String.class).get());
-		assertEquals("Intel64 Family 6 Model 142 Stepping 12, GenuineIntel", underTest.get("PROCESSOR_IDENTIFIER", String.class).get());
-		assertEquals("C:\\Program Files (x86)", underTest.get("ProgramFiles(x86)", String.class).get());
-		assertEquals("\\", underTest.get("file.separator", String.class).get());
+		assertEquals("4PTSOLUTIONS", underTest.get("USERDOMAIN_ROAMINGPROFILE", String.class).get());
+		//Number can always be string as well
+		assertThat(underTest.get("java.specification.version", String.class).get(), is(instanceOf(String.class)));
+		assertEquals("UTF-8", underTest.get("file.encoding", String.class).get());
 	}
 }
