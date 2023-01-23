21b3877d95f1a7698ca60878716ff3122a1f9881
Rob McDougall <rmcdougawww@rogers.com>
2023-01-23 14:55:43 -0500

    Added some comments to NamespacedContext.

--- formspipeline/src/main/java/com/_4point/aem/formspipeline/contexts/NamespacedContext.java
+++ formspipeline/src/main/java/com/_4point/aem/formspipeline/contexts/NamespacedContext.java
@@ -4,17 +4,38 @@ import java.util.Optional;
 
 import com._4point.aem.formspipeline.api.Context;
 
+/**
+ * NamespacedContext class - used to ensure uniqueness of key names without having to duplicate
+ * the namespace prefix throughout the code.
+ * 
+ * This class will be used in ContextWriters to ensure that the entries for this context have a common prefix.
+ * ContextReaders must prepend the namespace in order to read the values from a NamespacedContext. 
+ *
+ */
 public class NamespacedContext implements Context {
 	private static final String DEFAULT_NAMESPACE_SEPARATOR = "."; 
 	
 	private final String prefix;
 	private final Context originalContext;
 	
+	/**
+	 * Create the NamespacedContext.
+	 * 
+	 * @param namespace  namespace String
+	 * @param nsSeparator string used to separate the namespace from the key
+	 * @param originalContext original context
+	 */
 	public NamespacedContext(String namespace, String nsSeparator, Context originalContext) {
 		this.prefix = namespace + nsSeparator;
 		this.originalContext = originalContext;
 	}
 
+	/**
+	 * Create the NamespacedContext with a default separator (.)
+	 * 
+	 * @param namespace namespace String
+	 * @param originalContext original context
+	 */
 	public NamespacedContext(String namespace, Context originalContext) {
 		this(namespace, DEFAULT_NAMESPACE_SEPARATOR, originalContext);
 	}
