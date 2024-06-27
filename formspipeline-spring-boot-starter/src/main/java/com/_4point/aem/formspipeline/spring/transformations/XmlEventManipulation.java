60e91354a28ab66e7510b0175d6c66896c917b53
Rob McDougall <rmcdougawww@rogers.com>
2024-06-27 17:38:14 -0400

    ♻️ Further re-arrangement of code in prep for XmlAggregation
    Trying to keep as much code in the XmlEventManipulation as possible so
    that it can be shared with the (future) XmlAggregationTransformation.

--- formspipeline-spring-boot-starter/src/main/java/com/_4point/aem/formspipeline/spring/transformations/XmlEventManipulation.java
+++ formspipeline-spring-boot-starter/src/main/java/com/_4point/aem/formspipeline/spring/transformations/XmlEventManipulation.java
@@ -7,6 +7,8 @@ import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 import javax.xml.namespace.NamespaceContext;
 import javax.xml.stream.XMLEventReader;
@@ -33,32 +35,83 @@ abstract class XmlEventManipulation {
 		}
 	}
 
+	protected enum State {
+		InPreamble,				// Beginning of file up to the point where transactions occur
+		BeginTransaction,		// Start/inside a transaction
+		EndTransaction,			// End element of a transaction
+		BetweenTransactions,	// Character data, etc. outside of a transaction
+		InPostamble;			// After the end of the list transaction until the end of the file
+
+		// Checks to see if the current state should be changed.
+		private static State checkState(State currentState, boolean isStartElementEvent, boolean isEndElementEvent, int currentDepth, int transactionDepth) {
+			return switch (currentState) {
+			case InPreamble -> currentDepth <= transactionDepth ? State.InPreamble : State.BeginTransaction;
+			case BeginTransaction -> isEndElementEvent && currentDepth == transactionDepth + 1 ? State.EndTransaction : State.BeginTransaction;
+			case EndTransaction  -> currentDepth <= transactionDepth ? (isEndElementEvent ? State.InPostamble : State.BetweenTransactions) : State.BeginTransaction;
+			case BetweenTransactions -> isStartElementEvent ? (currentDepth <= transactionDepth ? State.InPostamble : State.BeginTransaction) : State.BetweenTransactions;  
+			case InPostamble -> State.InPostamble;
+			};
+		}
+	}
+	
+	protected static TransactionInfo convertToTransactions(List<EventInfo> eventList, int transactionDepth) {
+		List<EventInfo> preambleList = null;
+		List<EventInfo> postAmbleList;
+		List<List<EventInfo>> transactionList = new ArrayList<>(1024);
+		State currentState = State.InPreamble;
+		int transactionStartIndex = 0;
+		for (int i = 0; i < eventList.size(); i++) {
+			EventInfo currentEvent = eventList.get(i);
+			State newState = State.checkState(currentState, currentEvent.xmlEvent().isStartElement(), currentEvent.xmlEvent().isEndElement(), currentEvent.depth(), transactionDepth);
+			if (currentState != newState) {
+				if (currentState == State.InPreamble && newState == State.BeginTransaction) {
+					// Hit first transaction, so save what we've processed so far to preamble.
+					preambleList = eventList.subList(0, i);
+					transactionStartIndex = i;
+				} else if (currentState == State.BeginTransaction && newState == State.EndTransaction) {
+					// We've completed a transaction, so store it away.
+					transactionList.add(eventList.subList(transactionStartIndex, i));
+					transactionStartIndex = i;
+				} else if ((currentState == State.EndTransaction || currentState == State.BetweenTransactions) && (newState == State.InPostamble || newState == State.BetweenTransactions|| newState == State.BeginTransaction)) {
+					// We are between transactions and so, store the incoming data
+					transactionStartIndex = i;
+				} else if (currentState == State.BetweenTransactions && newState == State.BeginTransaction) {
+					// We have started a new state, stop ignoring characters
+				} else {
+					throw new IllegalStateException("Unexpected State transition from '" + currentState.toString() + "' to '" + newState.toString() + "'.");
+				}
+				currentState = newState;
+			}
+		}
+		postAmbleList = eventList.subList(transactionStartIndex, eventList.size());
+		
+		return new TransactionInfo(preambleList, transactionList, postAmbleList);
+	}
+
 	protected record TransactionInfo(List<EventInfo> preamble, List<List<EventInfo>> transactions, List<EventInfo> postamble) {
 		
-		byte[] replayTransactions(int transactionNumber) throws XMLStreamException, IOException {
+		byte[] replayTransaction(BiConsumer<TransactionInfo, AutoCloseableXmlEventWriter> writingLogic) throws XMLStreamException, IOException {
 			ByteArrayOutputStream os = new ByteArrayOutputStream();
 			// Create ByteArrayOuputStream to capture output, OutputStreamWriter to force UTF-8 output, then XMLEventWriter that is AutoCloseable 
 			try (os; var osw = new OutputStreamWriter(os, StandardCharsets.UTF_8); var ew = new AutoCloseableXmlEventWriter(xmlOutputFactory.createXMLEventWriter(osw))) {
-				writePreamble(ew);
-				writeTransaction(ew, transactionNumber);
-				writePostamble(ew);
+				writingLogic.accept(this, ew);
 			}
 			return os.toByteArray();
 		}
-		
-		private TransactionInfo writePreamble(XMLEventWriter ew) throws XMLStreamException {
+
+		protected TransactionInfo writePreamble(XMLEventWriter ew) throws XMLStreamException {
 			EventInfo.writeList(ew, preamble);
 			return this;
 		}
 
-		private TransactionInfo writeTransaction(XMLEventWriter ew, int transactionNumber) throws XMLStreamException {
+		protected TransactionInfo writeTransaction(XMLEventWriter ew, int transactionNumber) throws XMLStreamException {
 			List<EventInfo> eventList = transactions.get(transactionNumber);
 			logger.atDebug().addArgument(eventList.size()).addArgument(transactionNumber).log("Replaying {} events for transaction #{}.");
 			EventInfo.writeList(ew, eventList);
 			return this;
 		}
 
-		private TransactionInfo writePostamble(XMLEventWriter ew) throws XMLStreamException {
+		protected TransactionInfo writePostamble(XMLEventWriter ew) throws XMLStreamException {
 			EventInfo.writeList(ew, postamble);
 			return this;
 		}
--- formspipeline-spring-boot-starter/src/main/java/com/_4point/aem/formspipeline/spring/transformations/XmlSplittingTransformation.java
+++ formspipeline-spring-boot-starter/src/main/java/com/_4point/aem/formspipeline/spring/transformations/XmlSplittingTransformation.java
@@ -1,8 +1,6 @@
 package com._4point.aem.formspipeline.spring.transformations;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Stream;
 import java.util.stream.Stream.Builder;
 
@@ -14,6 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import com._4point.aem.formspipeline.api.DataTransformation.DataTransformationOneToMany;
 import com._4point.aem.formspipeline.spring.chunks.XmlDataChunk;
+import com._4point.aem.formspipeline.spring.transformations.XmlEventManipulation.AutoCloseableXmlEventWriter;
 
 /**
  * Splits an XmlChunk into multiple XmlChunks.
@@ -39,7 +38,8 @@ public class XmlSplittingTransformation extends XmlEventManipulation implements 
 			Builder<XmlDataChunk> streamBuilder = Stream.builder();
 			int transactionsSize = transactionInfo.transactions().size();
 			for (int i = 0; i < transactionsSize; i++) {
-				byte[] replayTransactions = transactionInfo.replayTransactions(i);
+				final int index = i;
+				byte[] replayTransactions = transactionInfo.replayTransaction((t,w)->writeTransactionWithPrePostAmble(index, t, w));
 				streamBuilder.accept(XmlDataChunk.create(replayTransactions, dataChunk.dataContext()));
 			}
 			return streamBuilder.build();
@@ -47,58 +47,14 @@ public class XmlSplittingTransformation extends XmlEventManipulation implements 
 			throw new IllegalStateException("Failed to split XML file.  %s".formatted(e.getMessage()), e);
 		} 
 	}
-
-	enum State {
-		InPreamble,				// Beginning of file up to the point where transactions occur
-		BeginTransaction,		// Start/inside a transaction
-		EndTransaction,			// End element of a transaction
-		BetweenTransactions,	// Character data, etc. outside of a transaction
-		InPostamble;			// After the end of the list transaction until the end of the file
-
-		// Checks to see if the current state should be changed.
-		private static State checkState(State currentState, boolean isStartElementEvent, boolean isEndElementEvent, int currentDepth, int transactionDepth) {
-			return switch (currentState) {
-			case InPreamble -> currentDepth <= transactionDepth ? State.InPreamble : State.BeginTransaction;
-			case BeginTransaction -> isEndElementEvent && currentDepth == transactionDepth + 1 ? State.EndTransaction : State.BeginTransaction;
-			case EndTransaction  -> currentDepth <= transactionDepth ? (isEndElementEvent ? State.InPostamble : State.BetweenTransactions) : State.BeginTransaction;
-			case BetweenTransactions -> isStartElementEvent ? (currentDepth <= transactionDepth ? State.InPostamble : State.BeginTransaction) : State.BetweenTransactions;  
-			case InPostamble -> State.InPostamble;
-			};
-		}
-	}
 	
-	private static TransactionInfo convertToTransactions(List<EventInfo> eventList, int transactionDepth) {
-		List<EventInfo> preambleList = null;
-		List<EventInfo> postAmbleList;
-		List<List<EventInfo>> transactionList = new ArrayList<>(1024);
-		State currentState = State.InPreamble;
-		int transactionStartIndex = 0;
-		for (int i = 0; i < eventList.size(); i++) {
-			EventInfo currentEvent = eventList.get(i);
-			State newState = State.checkState(currentState, currentEvent.xmlEvent().isStartElement(), currentEvent.xmlEvent().isEndElement(), currentEvent.depth(), transactionDepth);
-			if (currentState != newState) {
-				if (currentState == State.InPreamble && newState == State.BeginTransaction) {
-					// Hit first transaction, so save what we've processed so far to preamble.
-					preambleList = eventList.subList(0, i);
-					transactionStartIndex = i;
-				} else if (currentState == State.BeginTransaction && newState == State.EndTransaction) {
-					// We've completed a transaction, so store it away.
-					transactionList.add(eventList.subList(transactionStartIndex, i));
-					transactionStartIndex = i;
-				} else if ((currentState == State.EndTransaction || currentState == State.BetweenTransactions) && (newState == State.InPostamble || newState == State.BetweenTransactions|| newState == State.BeginTransaction)) {
-					// We are between transactions and so, store the incoming data
-					transactionStartIndex = i;
-				} else if (currentState == State.BetweenTransactions && newState == State.BeginTransaction) {
-					// We have started a new state, stop ignoring characters
-				} else {
-					throw new IllegalStateException("Unexpected State transition from '" + currentState.toString() + "' to '" + newState.toString() + "'.");
-				}
-				currentState = newState;
-			}
+	private void writeTransactionWithPrePostAmble(int transactionNumber, TransactionInfo ti, AutoCloseableXmlEventWriter ew) {
+		try {
+			ti.writePreamble(ew);
+			ti.writeTransaction(ew, transactionNumber);
+			ti.writePostamble(ew);
+		} catch (XMLStreamException e) {
+			throw new IllegalStateException("Failed to construct split XML file.  %s".formatted(e.getMessage()), e);
 		}
-		postAmbleList = eventList.subList(transactionStartIndex, eventList.size());
-		
-		return new TransactionInfo(preambleList, transactionList, postAmbleList);
 	}
-
 }
